0896dab02017d1874a8fa87e3864952b0e33e7d9
Jeisson Ortega <jeissonortegaunal2015@gmail.com>
2022-10-05 18:07:01 +0200

    Create numeroRandomBucleFor.py
    Creando numero Random utilizando el bucle For.

---
+++ numeroRandomBucleFor.py
@@ -0,0 +1,15 @@
+import random
+def numeroRandom():
+    numeroCinco=False
+    for i in range(3):
+        dado=random.randrange(1,10)
+        print(f'Tirada número {i+1} = {dado}')
+        if dado==5:
+            numeroCinco==True
+    if numeroCinco:
+        print('Ha sacado por lo menos un Cinco =)')
+    else:
+        print('Debe seguir intentando =(')
+    return 'Fin del programa'
+print(numeroRandom())        
+
